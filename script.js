2ff6ef8c105d858f307ee0c89c7765efb5eacd94
nammannam <56764231+nammannam@users.noreply.github.com>
2019-10-19 21:29:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let name = prompt('Your name');
+document.write('Hi ' + name);
+
+let year = prompt('Your birth');
+if (year > 2019) document.writeln('Sai');
+console.log('Your age is ' + (2019 - year));
